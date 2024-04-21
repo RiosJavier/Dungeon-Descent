1cbfd12749d99b9914fcee221a49272a0b579522
NoeSoto <auy2@txstate.edu>
2024-04-21 02:32:19 -0500

    Added freeze and continue functionality to shopkeeper room.

--- Assets/_Scripts/LoaderBorder.cs
+++ Assets/_Scripts/LoaderBorder.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class LoaderBorder : MonoBehaviour
 {
     public List<GameObject> prefabList;
+    public GameObject shopkeeperRoom;
     public static int roomCount;
+    public static int shopkeeperCount;
 
     void Start(){
         Debug.Log("room count set to 0");
-        roomCount = 0;
+        roomCount = 9;
+        shopkeeperCount = 0;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Debug.Log(collision.tag);
-
+        
         if (collision.tag == "Loader")
         {
             collision.tag = "already touched";
-            //Load in next room area.
-            Instantiate(prefabList[Random.Range(0, prefabList.Count)], new Vector3(transform.position.x+39,0,0), Quaternion.identity);
-            Debug.Log("next room loaded!");
             roomCount++;
+
+            if (roomCount % 10 == 0) 
+            {
+                //load the shopkeeper every ten rooms instead
+                Instantiate(shopkeeperRoom, new Vector3(transform.position.x + 39, 0, 0), Quaternion.identity);
+                shopkeeperCount++;
+                Debug.Log("Shopkeeper Loaded!!!!!");
+            }
+            else
+            {
+                //Load in next room area at random.
+                Instantiate(prefabList[Random.Range(0, prefabList.Count)], new Vector3(transform.position.x+39,0,0), Quaternion.identity);
+                Debug.Log("next room loaded!");
+            }
+        }
+
+        if(collision.tag == "Freezer")
+        {
+            collision.tag = "already touched";
+            roomCount++;
+
+            cameraMove.speed = 0f;
         }
     }
 }
--- Assets/_Scripts/cameraMove.cs
+++ Assets/_Scripts/cameraMove.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class cameraMove : MonoBehaviour
 {
-    public float speed;
+    public static float speed;
+
+    private void Start()
+    {
+        speed = 2f;
+    }
+
     void Update()
     {
         transform.Translate(Vector3.right * speed * Time.deltaTime);
